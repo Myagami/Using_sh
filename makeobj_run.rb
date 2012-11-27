31cb282ae5e99b18239589ebc5c007d823f7306c
masayoshi <masayoshi@ubuntu-devel.(none)>
2012-11-27 15:48:39 +0900

    makeobj simple command line tool

---
+++ makeobj_run.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby
+# -*- coding: utf-8 -*-
+
+#use dataSpace
+mode = 0
+dat = 0
+
+print "-----Makeobj Supporter For Ruby -----\n" 
+print "----- 1.Makeobj Working Mode Setting -----\n"
+print "----- 1.Paking 2.Other -----\n" 
+
+mode = STDIN.gets ;
+print mode+"\n"
