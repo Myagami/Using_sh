fab3dcb9b31568ad9d580dff3c769f9da0412816
M.Yagami <hayate@Reinforce-Eins.(none)>
2014-08-28 22:03:38 +0900

    update

--- Auto_Makeobj.sh
+++ Auto_Makeobj.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 PAK=$1
 DAT=$2
-## datをモニターする
-#inotifywait -m -e modify,create,delete --format %w%f Miyazaki_Prefectual_Office.dat |
-inotifywait -m -e modify,create,delete --format %w%f $DAT |
+PNG=$3
+## png / datをモニターする
+inotifywait -m -e modify,create,delete --format %w%f $PNG $DAT|
+#inotifywait -m -e modify,create,delete --format %w%f $DAT|
 while read files;do
     makeobj pak $PAK $DAT ;
     cp $PAK ~/simutrans/addons/pak.dev
     echo "--------------------------------"
+    #/home/hayate/simutrans/simutrans.start.112-3-Dev.sh
 done
 
--- ubuntu-dev.mount.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-mount -t cifs -o username=masayoshi,password=scarlet,defaults //192.168.0.4/masayoshi /mnt/macmini-dev
